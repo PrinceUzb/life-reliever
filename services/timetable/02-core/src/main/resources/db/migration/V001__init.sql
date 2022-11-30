23b09234684a4a016c03361776556dc1e8d9bcb7
Prince <Prince777_98@mail.ru>
2022-11-30 14:23:50 +0500

    tables

--- services/timetable/02-core/src/main/resources/db/migration/V001__init.sql
+++ services/timetable/02-core/src/main/resources/db/migration/V001__init.sql
@@ -12,6 +12,40 @@ CREATE TABLE IF NOT EXISTS timetable.academy (
     id UUID PRIMARY KEY,
     name VARCHAR NOT NULL UNIQUE,
     kind VARCHAR NOT NULL CONSTRAINT fk_academy_kind REFERENCES timetable.academy_kind (name) ON UPDATE CASCADE ON DELETE NO ACTION,
-    created_at timestamp NOT NULL,
+    created_at TIMESTAMP NOT NULL,
     deleted BOOLEAN NOT NULL DEFAULT false
   );
+
+CREATE TABLE IF NOT EXISTS timetable.groups (
+    id UUID PRIMARY KEY,
+    name VARCHAR NOT NULL UNIQUE,
+    students_count INT NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    deleted BOOLEAN NOT NULL DEFAULT false
+  );
+
+CREATE TABLE IF NOT EXISTS timetable.mentors (
+    id UUID PRIMARY KEY,
+    firstname VARCHAR NOT NULL,
+    lastname VARCHAR NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    deleted BOOLEAN NOT NULL DEFAULT false
+  );
+
+CREATE TABLE IF NOT EXISTS timetable.students (
+    id UUID PRIMARY KEY,
+    firstname VARCHAR NOT NULL,
+    lastname VARCHAR NOT NULL,
+    group_id UUID NOT NULL CONSTRAINT fk_groups REFERENCES timetable.groups (id) ON UPDATE CASCADE ON DELETE NO ACTION,
+    created_at TIMESTAMP NOT NULL,
+    deleted BOOLEAN NOT NULL DEFAULT false
+  );
+
+CREATE TABLE IF NOT EXISTS timetable.lessons (
+    id UUID PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    deleted BOOLEAN NOT NULL DEFAULT false
+  );
+
+
